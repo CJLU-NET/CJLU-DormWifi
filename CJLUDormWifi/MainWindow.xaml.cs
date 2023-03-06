ca0092142e79d3c0cc1145fa7d0b797771de8521
FoskyM <i@mouse123.cn>
2023-03-06 21:14:42 +0800

    添加项目文件。

---
+++ CJLUDormWifi/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+
+namespace CJLUDormWifi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+            ConfigurationManager.AppSettings["ServerIP"];
+            ConfigurationManager.AppSettings["ServerIP"];
+            ConfigurationManager.AppSettings["ServerIP"];
+            ConfigurationManager.AppSettings["ServerIP"];
+        }
+
+        private void Btn_Login_Click(object sender, RoutedEventArgs e)
+        {
+            Btn_Login.IsEnabled = false;
+            Btn_Login.Content = "登录中";
+        }
+    }
+}
